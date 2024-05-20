f1c0ab7e1dd2a2a435bba1f853c8408c7f18f21e
Sergeiphantom86 <96051914+Sergeiphantom86@users.noreply.github.com>
2024-05-21 02:22:27 +0300

    Explosion.cs

--- Explosion.cs
+++ Explosion.cs
@@ -1,22 +1,16 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
-    [SerializeField] private float _radius = 10;
-    [SerializeField] private float _force = 100;
-    
-    public void GetExplode()
+    [SerializeField] private float _explosionRadius = 2;
+    [SerializeField] private float _explosionForce = 100;
+
+    public void UseExplosion(List<Rigidbody> RigidbodysNewCubes)
     {
-        Collider[] overlappedCollider = Physics.OverlapSphere(transform.position, _radius);
-
-        for (int i = 0; i < overlappedCollider.Length; i++)
+        foreach(var RigidbodyNewCube in RigidbodysNewCubes)
         {
-            Rigidbody rigidbody = overlappedCollider[i].attachedRigidbody;
-
-            if (rigidbody)
-            {
-                rigidbody.AddExplosionForce(_force, transform.position, _radius);
-            }
+            RigidbodyNewCube.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
         }
     }
 }
