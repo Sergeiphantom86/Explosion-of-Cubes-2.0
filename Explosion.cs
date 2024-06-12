23caf0397f198565a12b49f0bfde8dd2e8a3a1f8
Sergeiphantom86 <96051914+Sergeiphantom86@users.noreply.github.com>
2024-06-12 14:11:22 +0300

    Update Explosion.cs

--- Explosion.cs
+++ Explosion.cs
@@ -3,14 +3,54 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
-    [SerializeField] private float _explosionRadius = 2;
-    [SerializeField] private float _explosionForce = 100;
+    private int _explosionForce;
+    private static int _temporaryForce = 50;
 
-    public void UseExplosion(List<Rigidbody> RigidbodysNewCubes)
+    private int _explosionRadius;
+    private static int _temporaryRadius = 2;
+
+    private static int _magnificatFactor = 2;
+    private int _height = 2;
+
+    private void Start()
     {
-        foreach(var RigidbodyNewCube in RigidbodysNewCubes)
+        _explosionRadius = _temporaryRadius;
+        _explosionForce = _temporaryForce;
+    }
+
+    public void UseAllObjects()
+    {
+        Collider[] overlappedColliders = Physics.OverlapSphere(transform.position, _explosionRadius);
+
+        foreach (var overlappedCollider in overlappedColliders)
         {
-            RigidbodyNewCube.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
+            Rigidbody rigidbody = overlappedCollider.attachedRigidbody;
+
+            if (rigidbody)
+            {
+                rigidbody.AddExplosionForce(_explosionForce, transform.position, _explosionRadius, _height);
+            }
         }
+    }
+
+    public void UseOnlyForNewObjects(List<Rigidbody> RigidbodysNewCubes)
+    {
+        foreach (var RigidbodyNewCube in RigidbodysNewCubes)
+        {
+            RigidbodyNewCube.AddExplosionForce(_explosionForce, transform.position, _explosionRadius, _height);
+        }
+
+        IncreaseForce(_explosionForce * _magnificatFactor);
+        IncreaseRadius(_explosionRadius + _magnificatFactor);
+    }
+
+    public void IncreaseForce(int increase)
+    {
+        _temporaryForce = increase;
+    }
+
+    public void IncreaseRadius(int increase)
+    {
+        _temporaryRadius = increase;
     }
 }
